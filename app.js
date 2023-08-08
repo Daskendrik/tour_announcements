e5a23aee70e2fb8b970805c5b29fe9f7fed187af
Dashka_W <Daskendrik@users.noreply.github.com>
2023-08-08 17:57:01 +0300

    почистила код для картинок

--- app.js
+++ app.js
@@ -2,89 +2,48 @@
 class Gallery {
     constructor(id) {
         this.gallery = document.querySelector(`#${id}`)
-        this.upBtn = firstGallery.querySelector('.up-button')
-        this.downBtn = firstGallery.querySelector('.down-button')
-        this.sidebar = firstGallery.querySelector('.sidebar')
-        this.mainSlide = firstGallery.querySelector('.main-slide')
-        this.slidesCount = mainSlide.querySelectorAll('div').length
-        this.container = firstGallery.querySelector('.gallery')
+        this.upBtn = this.gallery.querySelector('.up-button')
+        this.downBtn = this.gallery.querySelector('.down-button')
+        this.sidebar = this.gallery.querySelector('.sidebar')
+        this.mainSlide = this.gallery.querySelector('.main-slide')
+        this.slidesCount = this.mainSlide.querySelectorAll('div').length
+        this.container = this.gallery.querySelector('.gallery')
+        this.sidebar.style.top = `-${(this.slidesCount-1) * 100}%`
+        this.activeSlideIndex = 0
     }
+
+    changeSlide(direction){
+        if(direction === 'up') {
+            this.activeSlideIndex++
+            if(this.activeSlideIndex === this.slidesCount) {
+                this.activeSlideIndex = 0
+            }
+        } else if(direction === 'down') {
+            this.activeSlideIndex--
+            if (this.activeSlideIndex < 0) {
+                this.activeSlideIndex = this.slidesCount - 1
+            }
+        }
+        console.log(this.slidesCount)
+        const height = this.container.clientHeight
+        this.mainSlide.style.transform = `translateY(-${this.activeSlideIndex * height}px)`
+        this.sidebar.style.transform = `translateY(${this.activeSlideIndex * height}px)`
+    }
+
 }
 
-// первая галлерея
-const firstGallery = document.querySelector('#firstGallery')
-let upBtn = firstGallery.querySelector('.up-button')
-let downBtn = firstGallery.querySelector('.down-button')
-
-let sidebar = firstGallery.querySelector('.sidebar')
-let mainSlide = firstGallery.querySelector('.main-slide')
-let slidesCount = mainSlide.querySelectorAll('div').length
-let container = firstGallery.querySelector('.gallery')
-
-let activeSlideIndex = 0
-
-sidebar.style.top = `-${(slidesCount-1) * 100}%`
-
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
+const firstGallery = new Gallery('firstGallery')
+firstGallery.upBtn.addEventListener('click', () => {
+    firstGallery.changeSlide('up')
 })
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
+firstGallery.downBtn.addEventListener('click', () => {
+    firstGallery.changeSlide('down')
 })
 
-function changeSlide(direction){
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if(direction === 'down') {
-        activeSlideIndex--
-        if (activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
-    }
-    console.log(slidesCount)
-    const height = container.clientHeight
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
-}
-
-// вторая галлерея
-const secondGallery = document.querySelector('#secondGallery')
-upBtn = secondGallery.querySelector('.up-button')
-downBtn = secondGallery.querySelector('.down-button')
-
-sidebar = secondGallery.querySelector('.sidebar')
-mainSlide = secondGallery.querySelector('.main-slide')
-slidesCount = mainSlide.querySelectorAll('div').length
-container = secondGallery.querySelector('.gallery')
-
-activeSlideIndex = 0
-
-sidebar.style.top = `-${(slidesCount-1) * 100}%`
-
-upBtn.addEventListener('click', () => {
-    changeSlide('up')
+const secondGallery = new Gallery('secondGallery')
+secondGallery.upBtn.addEventListener('click', () => {
+    secondGallery.changeSlide('up')
 })
-downBtn.addEventListener('click', () => {
-    changeSlide('down')
+secondGallery.downBtn.addEventListener('click', () => {
+    secondGallery.changeSlide('down')
 })
-
-function changeSlide(direction){
-    if(direction === 'up') {
-        activeSlideIndex++
-        if(activeSlideIndex === slidesCount) {
-            activeSlideIndex = 0
-        }
-    } else if(direction === 'down') {
-        activeSlideIndex--
-        if (activeSlideIndex < 0) {
-            activeSlideIndex = slidesCount - 1
-        }
-    }
-    console.log(slidesCount)
-    const height = container.clientHeight
-    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
-    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
-}
