87991660bd255170b9080a54acbf8dd41225cbca
Daskendrik <ss599@yandex.ru>
2023-08-12 10:57:26 +0300

    создала круг

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 
 
 const circle = document.querySelector('.circle')
+let myCircle;
 const tables = document.querySelectorAll('.table')
 let firstDay;
 let secondtDay;
 
 // table.addEventListener('scroll', seeCircle)
 
-function seeCircle(){
-    console.log('1111')
-    // circle.classList.contains('.hide-circle') ? circle.classList.remove('.hide-circle') : circle.classList.add('.hide-circle')
-}
+
 
 class TableTour {
   constructor(id,height, heightEnd){
@@ -20,12 +18,54 @@ class TableTour {
   }
 }
 
+class Circle {
+  constructor(id,height, heightEnd){
+    this.id = id
+    this.heightBegin = height
+    this.heightEnd = heightEnd
+  }
+}
 
-let num = 1;
-for(let table of tables) {
-  let r = table.getBoundingClientRect();
-  num==1 ? firstDay = new TableTour(num,r.y,r.bottom) : secondtDay = new TableTour(num,r.y,r.bottom)
-  num++
+function checkMyCircle(circle){
+  if(myCircle) {
+    reloadMyCircle(circle)
+  } else {
+    createNewMyCircle(circle)
+  }
+}
+
+function reloadMyCircle(circle){
+  let r = circle.getBoundingClientRect();
+  myCircle.heightBegin = r.y;
+  myCircle.heightEnd = r.bottom;
+}
+
+function createNewMyCircle(circle){
+  let r = circle.getBoundingClientRect();
+  myCircle = new Circle(num,r.y,r.bottom)
+}
+
+function checkObjTable(tables){
+  if(firstDay && secondtDay) {
+    reloadObjTable(tables)
+  } else {
+    createNewObjTable(tables)
+  }
+}
+
+function reloadObjTable(tables){
+  let num = 1;
+  for(let table of tables) {
+    let r = table.getBoundingClientRect();
+    if (num==1){
+      firstDay.heightBegin = r.y;
+      firstDay.heightEnd = r.bottom;
+    }else {
+      secondtDay.heightBegin = r.y;
+      secondtDay.heightEnd = r.bottom;
+    }
+    num++
+  }
 }
 
 function createNewObjTable(tables){
@@ -36,5 +76,8 @@ function createNewObjTable(tables){
   num++
   }
 }
-console.log(firstDay)
-console.log(secondtDay)
+
+window.addEventListener('scroll', () => {
+  checkObjTable(tables)
+
+})
