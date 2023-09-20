00a3f0907d2132107c93bf509d6306f93a7cec01
Daskendrik <ss599@yandex.ru>
2023-09-20 17:46:17 +0300

    add style menu

--- src/Components/Global/Menu.js
+++ src/Components/Global/Menu.js
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import styles from './Menu.module.css';
 
 const Menu = () => {
   return (
-    <>
+    <div className={styles.menu}>
       <NavLink to=".">Главная</NavLink>
       <NavLink to="tours">Предстоящие туры</NavLink>
       <NavLink to="backfeed">Обраная связь</NavLink>
-    </>
+    </div>
   );
 };
 
--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,4 +1,4 @@
-import Menu from './Menu';
+import Menu from './Global/Menu';
 import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
